301a53536828dce0b2c9ba7d450de87356fd883d
mschmit <mschmit@student.42.fr>
2015-04-13 13:46:15 +0200

    new header for client

---
+++ includes/client.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sho <sho@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/08 15:27:07 by sho               #+#    #+#             */
+/*   Updated: 2015/04/13 13:38:19 by sho              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "../includes/libft.h"
+
+
+#endif
